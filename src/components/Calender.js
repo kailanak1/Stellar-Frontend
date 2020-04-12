ba5fbd6d46614bef6c9d48b7dc6c1be5908233c7
clheard54 <clarion.heard@gmail.com>
2020-04-12 12:49:25 -0700

    page rendering succesfully

--- src/components/Calender.js
+++ src/components/Calender.js
@@ -1,34 +1,19 @@
-import React from 'react'
-import 'react-event-calendar'
-import UserEvent from './UserEvent'
+// import React from 'react'
+// import { Calendar, momentLocalizer } from 'react-big-calendar'
+// import moment from 'moment'
 
-const EventCalendar = (props) => {
+// const localizer = momentLocalizer(moment)
 
-    const events = [
-        {
-            start: '2020-04-12',
-            end: '2020-04-20',
-            eventClasses: 'optionalEvent',
-            title: 'Amazing meteor shower',
-            description: 'Stars be falling from the sky',
-        },
-        {
-            start: '2020-05-01',
-            end: '2020-05-01',
-            title: 'Explosion',
-            description: 'Ready for a supernova?',
-            data: 'maybe there are some random details you want to use later. invite yo\' friends.',
-        },
-    ];
+// const MyCalendar = props => (
+//   <div>
+//     <Calendar
+//       localizer={localizer}
+//       events={"hello"}
+//       startAccessor="start"
+//       endAccessor="end"
+//       style={{ height: 500 }}
+//     />
+//   </div>
+// )
 
-    return (
-        <div className='calendar'>
-            month={4}
-            year={2020}
-            events={events} 
-            onEventClick={(target, eventData, day) => console.log(eventData) }
-        </div>
-        )
-}
-
-export default EventCalendar;
+// export default MyCalendar
---
+++ src/components/ConstellationList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class ConstellationList extends React.Component {
+  render(){
+    return (
+        <h2>Constellations Galore</h2>
+      )
+    }
+}
