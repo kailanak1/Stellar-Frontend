887eb8b20a0a25c1fff036e8ae3cdc2a71b5e645
clheard54 <clarion.heard@gmail.com>
2020-04-15 15:48:50 -0700

    event list formatted

--- src/components/UserEvent.js
+++ src/components/UserEvent.js
@@ -2,6 +2,20 @@ import React from "react";
 import AuthHOC from "../HOCs/AuthHOC";
 import { api } from '../services/api'
 
+const months = {
+  '01': "JAN",
+  '02': "FEB",
+  '03': "MAR",
+  '04': "APR",
+  '05': "MAY",
+  '06': "JUN",
+  '07': "JUL",
+  '08': "AUG",
+  '09': "SEP",
+  '10': "OCT",
+  '11': "NOV", 
+  '12': "DEC" }
+
 class UserEvent extends React.Component{
   constructor(){
     super();
@@ -22,12 +36,15 @@ class UserEvent extends React.Component{
       if (this.state.events.length == 0){
         return <h3>You have no events on your calendar yet.</h3>
       } else {
+        let i=1
         return this.state.events.map(event => {
-          return (<li>
-                    <h3>{event.title}</h3>
-                    <p>{event.date} at {event.time}</p>
-                    <p>Notes: {event.details}</p>
-                  </li>)
+          return (<div>
+                    <h3>{i}. {event.title[0].toUpperCase() + event.title.slice(1)}</h3>
+                    <p>{`${months[event.date.slice(5,7)]} ${event.date.slice(8,10)}, ${event.date.slice(0,4)}`} at {event.time}<br></br>
+                    {i++}
+                    Things to Remember: {event.details}</p><br></br>
+                  </div>
+                  )
         })
       }
   }
@@ -36,9 +53,7 @@ class UserEvent extends React.Component{
     return (
       <div className="events">
         <h2>My Events:</h2>
-        <ul>
         {this.renderEvents()}
-        </ul>
       </div>
     );
   }
